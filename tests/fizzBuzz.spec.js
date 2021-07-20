0eefe4e99af818f3271a0d8a382ab2c318b1fd2b
jfirme-sys <julianofirme23@gmail.com>
2021-07-19 23:50:59 -0300

    TRIANGULATE - FizBuzz returns 2

--- tests/fizzBuzz.spec.js
+++ tests/fizzBuzz.spec.js
@@ -5,4 +5,8 @@ describe("FizzBuzz", () => {
     it("FizzBuzz returns 1", () => {
         expect(fizzBuzz.fizzBuzz(1)).to.equal(1);
     });
+
+    it("FizzBuzz returns 2", () => {
+        expect(fizzBuzz.fizzBuzz(2)).to.equal(2);
+    });
 });
